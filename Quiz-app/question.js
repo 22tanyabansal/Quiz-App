0bd8477ccee1774b8d797443ab19033137a89996
Tanya Bansal <91904607+22tanyabansal@users.noreply.github.com>
2022-07-31 15:26:51 +0530

    Add files via upload

---
+++ Quiz-app/question.js
@@ -0,0 +1,61 @@
+var questions = [
+    {
+        numb: 1,
+        question: "Which state produces maximum soybean?",
+        answer: "Madhya Pradesh",
+        option: [
+            "Madhya Pradesh",
+            "Uttar Pradesh",
+            " Bihar",
+            "Rajasthan",
+        ]
+    },
+
+    {
+        numb: 2,
+        question: "Which country operationalized world's largest radio telescope?",
+        answer: "China",
+        option: [
+            "USA",
+            "China",
+            "Russia",
+            "India",
+        ]
+    },
+
+    {
+        numb: 3,
+        question: "Which of the following is the capital of Arunachal Pradesh?",
+        answer: "Itanagar",
+        option: [
+            "Itanagar",
+            "Dispur",
+            "Imphal",
+            "Panaji",
+        ]
+    },
+
+    {
+        numb: 4,
+        question: " Which one among the following radiations carries maximum energy?",
+        answer: "Gamma rays",
+        option: [
+            "Ultraviolet rays",
+            "Gamma rays",
+            "X-rays",
+            "Infra-red rays",
+        ]
+    },
+
+    {
+        numb: 5,
+        question: "Bokaro Steel Limited was established with the assistance of",
+        answer: "Soviet Union",
+        option: [
+            "Germany",
+            "Soviet Union",
+            "UK",
+            "USA"
+        ]
+    },
+];
